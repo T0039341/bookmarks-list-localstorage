b0adb7012d679ed0e23c3324d0982b84a4141756
T0039341 <t.hojeiri@gmail.com>
2025-08-08 11:45:44 +0100

    Add comment to check

--- src/app/edit-modal/edit-modal.component.ts
+++ src/app/edit-modal/edit-modal.component.ts
@@ -60,7 +60,7 @@ export class EditModalComponent implements OnInit, AfterViewChecked {
   get url() {
     return this.form.get('url');
   }
-  //receive edit icon click event
+  //receive edit icon click event something
   getEditClickEvent() {
     this.sharedService.currentDataStream.subscribe(res => {
       if (res === true) {
